629ef86f095ce5461f05aab6a196b53879ca831f
hhriyy <heorhiiiukrainian2006@gmail.com>
2025-12-03 04:46:20 +0200

    init commit

---
+++ include/WINtrrt.h
@@ -0,0 +1,15 @@
+﻿#ifndef WINTRRT_H
+#define WINTRRT_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#endif
---
+++ src/WINtrrt.c
@@ -0,0 +1,33 @@
+﻿#include "WINtrrt.h"
+
+int main(int argc, char** argv)
+{
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s == INVALID_SOCKET) {
+        printf("socket error: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    ZeroMemory(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(40001);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    const char* msg = "hello";
+
+    int sent = sendto(s, msg, strlen(msg), 0, (struct sockaddr*)&addr, sizeof(addr));
+
+    if (sent == SOCKET_ERROR) {
+        printf("sendto error: %d\n", WSAGetLastError());
+    }
+    else {
+        printf("sent %d bytes\n", sent);
+    }
+
+    closesocket(s);
+    WSACleanup();
+}
